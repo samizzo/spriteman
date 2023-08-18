29b9cf2e7b1d582675dc1bb20cdbfc463e71280f
Sam Izzo <sam@humbug.net>
2023-08-19 00:02:00 +1000

    Added sprite class.

---
+++ Sprite.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace spriteman
+{
+    internal class Sprite
+    {
+        public string Name { get; set; }
+        public Dictionary<string, string> Kvps { get; set; }
+
+        public Sprite()
+        {
+            Kvps = new Dictionary<string, string>();
+        }
+    }
+}
--- SpriteProject.cs
+++ SpriteProject.cs
@@ -5,10 +5,12 @@ namespace spriteman
     internal class SpriteProject
     {
         public List<string> Images { get; }
+        public List<Sprite> Sprites { get; }
 
         public SpriteProject()
         {
             Images = new List<string>();
+            Sprites = new List<Sprite>();
         }
     }
 }
